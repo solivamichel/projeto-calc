c19102ddf8ac13141a9aec392b4f3d17679997fc
Michel Soliva <michelsolivadev@gmail.com>
2020-08-03 23:20:03 -0300

    Tratamento de Exceções

--- src/main/java/br/com/soliva/MathController.java
+++ src/main/java/br/com/soliva/MathController.java
@@ -5,14 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.soliva.exception.UnsuportedMathOperationException;
+
 @RestController
 public class MathController {
 	
 	
-	@RequestMapping(value = "/sum/numberOne/numberTwo", method = RequestMethod.GET)
+	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
 	public Double sum( @PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo ) throws Exception {
 		if( !isNumeric(numberOne) || !isNumeric(numberTwo) ) {
-			throw new Exception();
+			throw new UnsuportedMathOperationException("Please set a numeric value! ");
 		}
 		
 		Double sum = convertDouble(numberOne) + convertDouble(numberTwo);
@@ -29,6 +31,6 @@ public class MathController {
 	private boolean isNumeric( String strNumber ) {
 		if( strNumber == null) return false;
 		String number = strNumber.replaceAll(",", ".");
-		return number.matches("[-+]?[0-9]*\\\\.?[0-9]+");
+		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
 	}
 }
